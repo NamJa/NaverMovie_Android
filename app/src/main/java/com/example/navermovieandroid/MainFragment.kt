9fb632b1e36ccad8a14240b5cc53512361261743
Jongwoo Kim <kjwoo810@gmail.com>
2022-05-18 22:18:55 +0900

    MainFragment UI 구성 & RecyclerView 아이템 UI 구성

---
+++ app/src/main/java/com/example/navermovieandroid/MainFragment.kt
@@ -0,0 +1,31 @@
+package com.example.navermovieandroid
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+}
