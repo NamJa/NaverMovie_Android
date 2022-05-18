7d7140e02a4aff74504ea29bdc8ea44c01a541c1
Jongwoo Kim <kjwoo810@gmail.com>
2022-05-18 23:11:32 +0900

    editText Enter키 동작 정의

--- app/src/main/java/com/example/navermovieandroid/MainFragment.kt
+++ app/src/main/java/com/example/navermovieandroid/MainFragment.kt
@@ -1,13 +1,20 @@
 package com.example.navermovieandroid
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
 
 
 class MainFragment : Fragment() {
+
+    private lateinit var inputLayout: TextInputLayout
+    private lateinit var editText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +29,23 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView(view)
+
+        // editText에서 enter키를 입력받았을 경우
+        editText.setOnKeyListener(object :View.OnKeyListener{
+            override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
+                if(keyCode == KeyEvent.KEYCODE_ENTER || keyCode == KeyEvent.KEYCODE_SEARCH) {
+                    // testcode
+                    Toast.makeText(requireContext(), editText.text, Toast.LENGTH_SHORT).show()
+                }
+                return true
+            }
+        })
+    }
+
+    private fun initView(view: View){
+        inputLayout = view.findViewById(R.id.tiLayout)
+        editText = inputLayout.editText!!
     }
 
     companion object {
